ac7191f0fb9719400d12fde4f5a579e181d0701f
Emmanuel <73722390+oyeranmiemmanuel@users.noreply.github.com>
2026-09-06 20:56:49 +0100

    Delete apps directory

--- apps/core/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .references import generate_reference
-from .slugs import unique_slugify
-
-__all__ = ["generate_reference", "unique_slugify"]
--- apps/orders/services/__init__.py
+++
@@ -1,3 +0,0 @@
-from .checkout import create_order_from_cart
-
-__all__ = ["create_order_from_cart"]
